9aff4a343568031f5392fd88e8712a55e167858e
Sabeenah O <45824984+sabbiebaz@users.noreply.github.com>
2020-05-09 19:36:38 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculatorweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
